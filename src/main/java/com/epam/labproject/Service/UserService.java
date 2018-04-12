6e2b8c22c1e3302c4ecc6fdf3db4c2e8052cb677
fedorBrovkin <brovkin_fedor@mail.ru>
2018-04-13 01:35:40 +0300

    12

--- src/main/java/com/epam/labproject/Service/UserService.java
+++ src/main/java/com/epam/labproject/Service/UserService.java
@@ -1,20 +1,32 @@
 package com.epam.labproject.Service;
 
 import com.epam.labproject.entity.User;
+import com.epam.labproject.repository.RoleRepository;
 import com.epam.labproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
     private UserRepository userRepository;
+    private RoleRepository roleRepository;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository,RoleRepository roleRepository,
+                       BCryptPasswordEncoder bCryptPasswordEncoder){
+
         this.userRepository=userRepository;
+        this.roleRepository=roleRepository;
+        this.bCryptPasswordEncoder=bCryptPasswordEncoder;
     }
 
     public void save(User user){
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+
         userRepository.save(user);
     }
 }
