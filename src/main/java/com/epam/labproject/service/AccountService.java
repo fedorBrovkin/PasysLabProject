70bb76ecfd838135ffb293090d90711207725779
fedorBrovkin <brovkin_fedor@mail.ru>
2018-04-16 14:12:16 +0300

    mappedBy parameter added to List in UserEntity class

--- src/main/java/com/epam/labproject/service/AccountService.java
+++ src/main/java/com/epam/labproject/service/AccountService.java
@@ -4,22 +4,26 @@ package com.epam.labproject.service;
 import com.epam.labproject.model.entity.Account;
 import com.epam.labproject.model.entity.CreditCard;
 import com.epam.labproject.model.entity.Payment;
+import com.epam.labproject.model.entity.User;
 import com.epam.labproject.repository.AccountRepository;
+import com.epam.labproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Service
 public class AccountService {
     private AccountRepository accountRepository;
-
+    private UserService userService;
     @Autowired
-    public AccountService(AccountRepository accountRepository){
+    public AccountService(AccountRepository accountRepository, UserService userService){
         this.accountRepository=accountRepository;
+        this.userService=userService;
     }
 
     public void save(Account account){
@@ -29,4 +33,14 @@ public class AccountService {
     public Account findByNumber(int number){
         return accountRepository.findByNumber(number);
     }
+    public void createAccount(String login){
+     User user = userService.getUser(login);
+     Account account= new Account();
+     account.setBalance(new BigDecimal(1000));
+     account.setStatus(true);
+     account.setUser(user);
+     account.setDateOfCreation(LocalDateTime.now());
+     this.save(account);
+    }
+    
 }
